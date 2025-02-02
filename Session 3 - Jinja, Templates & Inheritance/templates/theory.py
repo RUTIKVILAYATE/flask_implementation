bdee70619a5e6531c1f75633fb0f15f86eece281
Rutik Vilayate <105170592+RUTIKVILAYATE@users.noreply.github.com>
2025-02-03 00:47:47 +0530

    Add files via upload

---
+++ Session 3 - Jinja, Templates & Inheritance/templates/theory.py
@@ -0,0 +1,5 @@
+# Custom HTML Templates
+# Jinja Template
+# Template Inheritance -> if content of more than one file is same -> we should use one files content in the other files by onheriting content 
+
+
